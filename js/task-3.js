334600326cb66ae793f6abaf770b0d8f92cd779f
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2023-10-25 07:24:00 +0300

    hw-1-js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,10 @@
 function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-
-  const elementWidth = contentWidth + 2 * (paddingWidth + borderWidth);
-  return elementWidth;
+  return (
+    Number.parseFloat(content) +
+    2 * (Number.parseFloat(padding) + Number.parseFloat(border))
+  );
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
